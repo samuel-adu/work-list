5bbff2a77270f762829d7d37a9a7c4d715199be7
Samuel Adu <theadusamuel@gmail.com>
2023-11-13 19:35:24 +0000

    chore: initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <h1>Work List</h1>
+      <ul>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+      </ul>
+    </>
+  );
+}
+
+export default App;
